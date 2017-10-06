f31ccf4b8929c281519d214bcd556d81f0080537
Matt Bierbaum <matt.bierbaum@gmail.com>
2017-10-06 18:02:15 -0400

    trying to optimize the fourier fits; no working

--- oned/fourier_register.py
+++ oned/fourier_register.py
@@ -1,7 +1,6 @@
 import numpy as np
 from scipy.optimize import golden, leastsq
 from matplotlib.pyplot import *
-from numpy.polynomial.chebyshev import Chebyshev
 from scipy.ndimage import gaussian_filter
 
 from leastsq import LM
@@ -9,6 +8,111 @@ from leastsq import LM
 DEGREE = 20
 
 rng = np.random.RandomState(14850)
+
+class SuperRegistration(object):
+    def __init__(self, images, deg, shifts=None):
+        """
+        Parameters
+        ----------
+        x : ndarray [L]
+            List of coordinates for the first image in the sequence.
+            It will be used for the basis of all other coordinates for the other images
+
+        y : ndarray [L,N]
+            All the images
+        """
+        self.deg = deg
+        self.images = images
+        self.shifts = shifts
+        self.N = len(self.images)
+        self.L = len(self.images[0])
+
+        if self.shifts is None:
+            self.shifts = rng.rand(len(self.images)-1)
+
+        self.coef = np.random.randn(2*deg)
+
+        self.x = np.arange(images[0].shape[0])
+        self.k = 2*np.pi*np.arange(self.deg)
+        self.domain = [self.x.min() - self.x.ptp(), self.x.max() + self.x.ptp()]
+        self.sinkx = np.sin(self.k[:,None] * self.coord(self.x)[None,:])
+        self.coskx = np.cos(self.k[:,None] * self.coord(self.x)[None,:])
+
+        self.gradcoef = np.vstack([
+            np.hstack([self.sinkx for i in range(self.N)]),
+            np.hstack([self.coskx for i in range(self.N)])
+        ])
+
+    def set_params(self, params):
+        self.shifts = params[:len(self.shifts)]
+        self.coef = params[len(self.shifts):]
+
+    @property
+    def params(self):
+        return np.hstack([self.shifts, self.coef])
+
+    @property
+    def model(self):
+        return np.array([self(self.x)] + [self(self.x + s) for s in self.shifts])
+
+    @property
+    def An(self):
+        return self.coef[:self.deg]
+
+    @property
+    def Bn(self):
+        return self.coef[self.deg:]
+
+    def coord(self, x):
+        return (x - self.domain[0]) / (self.domain[1] - self.domain[0])
+
+    def __call__(self, x):
+        kx = np.outer(self.coord(x), self.k)
+        return (self.An*np.sin(kx) + self.Bn*np.cos(kx)).sum(axis=-1)
+
+    def res(self, params=None):
+        params = params if params is not None else self.params
+        self.set_params(params)
+
+        return (self.model - self.images).ravel()
+
+    def gradshifts(self, shifts=None):
+        shifts = shifts if shifts is not None else self.shifts
+
+        sinks = np.sin(shifts[:,None] * self.k[None,:])
+        cosks = np.cos(shifts[:,None] * self.k[None,:])
+
+        dIds_sin = -(self.k*(self.An*sinks + self.Bn*cosks)).dot(self.sinkx)
+        dIds_cos = -(self.k*(self.Bn*sinks - self.An*cosks)).dot(self.coskx)
+        return dIds_sin + dIds_cos
+
+    def grad(self, params=None):
+        params = params if params is not None else self.params
+        self.set_params(params)
+
+        gshifts = self.gradshifts(self.shifts)
+        gradshifts = np.zeros((self.N-1, self.L*self.N))
+        for i in range(self.N-1):
+            gradshifts[i, (i+1)*self.L:(i+2)*self.L] = gshifts[i]
+
+        return np.vstack([gradshifts, self.gradcoef]).T
+
+    def jac(self, p, h=1e-6):
+        assert len(p) == len(self.params)
+        p0 = p.copy()
+        r0 = self.res(p0)
+        j = []
+        for i in range(len(p0)):
+            p0[i] += h
+            j += [(self.res(p0) - r0)/h]
+            p0[i] -= h
+        return np.array(j).T
+
+    def fit(self, **kwargs):
+        lm = LM(self.res, self.jac)
+        self.sol = lm.leastsq(self.params, **kwargs)
+        return self.sol[0][:self.N-1]
+
 
 class Fourier(object):
     def __init__(self, x, y, deg, domain=None, coef=None):
@@ -46,12 +150,6 @@ class Fourier(object):
         r = x.max() - x.min()
         return [x.min()-r, x.min() + r]
 
-@classmethod
-def default_cheb_domain(cls, x, **kwargs):
-    pad = kwargs.get('pad', x.ptp()/5)
-    return [x.min() - pad, x.max() + pad]
-
-Chebyshev.default_domain  = default_cheb_domain
 
 class OneDRegister(object):
     def __init__(self, seq, deg, shifts=None, interpolator=Fourier, pad=None,
@@ -177,7 +275,7 @@ def single_point_bias(N=2000, noise=0.05):
 
 if __name__=="__main__":
     degree = 37 # DEGREE
-    expt = fakedata(2, 124, 0.02, deg=40)
+    expt = fakedata(2, 124, 1e-4, deg=40)
     data, s, true, fourier = expt
     reg = OneDRegister(data, deg=degree)
 
