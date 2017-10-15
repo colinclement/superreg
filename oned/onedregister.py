c19bdef63be2d92f37690c6f9bd83406b03115d8
Colin Clement <colin.clement@gmail.com>
2017-10-15 15:28:57 -0400

    made interfaces match SuperRegistration, fixed initial guess

--- oned/onedregister.py
+++ oned/onedregister.py
@@ -16,7 +16,7 @@ except ImportError as ierr:
     print("Install pyfftw for 20x speedup")
     hasfftw = False
 import leastsq
-from scipy.optimize import golden
+from scipy.optimize import minimize_scalar
 from scipy.special import expit
 
 
@@ -31,7 +31,7 @@ class Register(object):
     infer delta we want to solve the following optimization problem:
         delta^* = min_delta 1/2 ||I0 - T_delta(I1)||^2
     """
-    def __init__(self, imag1, imag0, **kwargs):
+    def __init__(self, images, **kwargs):
         """
         Find the vector [dy, dx] which translates imag1 to match imag0.
 
@@ -47,16 +47,15 @@ class Register(object):
             reg = Register(imag1, imag0)
             delta, delta_sigma, msg = reg.fit()
         """
-        assert imag0.shape == imag1.shape, "Images must share their shape"
-        self.imag0 = imag0
-        self.imag1 = imag1
+        self.initialize(images)
+        assert self.imag0.shape == self.imag1.shape, "Images must share their shape"
         self.masktype = kwargs.get("masktype", "sigmoid")
         self._maskdict = {'linear': self._linear_interp_mask,
                           'constant': self._constant_region_mask,
                           'sigmoid': self._sigmoid_mask}
         self.mask_kwargs = kwargs.get('mask_kwargs', {})
 
-        self.L = len(imag0)
+        self.L = len(self.imag0)
         self._h = kwargs.get("h", 1E-7)
         self._mirror = kwargs.get("mirror", True)
 
@@ -65,11 +64,11 @@ class Register(object):
         # For mirror padding
         self.kx2 = np.fft.rfftfreq(2*self.L, d=1./(2*np.pi))
 
-        self.initialize(imag1)
-
-    def initialize(self, imag):
-        self.imag1_k = np.fft.rfft(imag)
-        self.imag1_mirror = self.mirrorpad(imag)
+    def initialize(self, images):
+        self.images = images
+        self.imag0, self.imag1 = images
+        self.imag1_k = np.fft.rfft(self.imag1)
+        self.imag1_mirror = self.mirrorpad(self.imag1)
         self.imag1_mirror_k = np.fft.rfft(self.imag1_mirror)
 
     def mirrorpad(self, imag):
@@ -95,7 +94,8 @@ class Register(object):
         else:
             imag_k = self.imag1_k if imag is None else np.fft.rfft(imag)
             kx = self.kx
-        return np.fft.irfft(imag_k * np.exp(-1.j * delta * kx))[:self.L]
+        # NOTE: I choose a convention for shifts here
+        return np.fft.irfft(imag_k * np.exp(1.j * delta * kx))[:self.L]
 
     def _linear_interp_mask(self, x, d, **kwargs):
         if d >= 0:
@@ -121,7 +121,7 @@ class Register(object):
         mask = self._maskdict[self.masktype]
         return mask(self.x, delta, **self.mask_kwargs)
 
-    def residual(self, delta, imag1=None, imag0=None):
+    def residual(self, delta, images=None):
         """
         Finds the difference translate(imag1) - imag0
         input:
@@ -135,32 +135,29 @@ class Register(object):
             residual : array_like, shape can be smaller than (Ly, Lx) if delta
                 has components larger in magnitude than 0.5
         """
-        imag1_delta = self.translate(delta, imag1)
-        imag0 = imag0 if imag0 is not None else self.imag0
+        if images is not None:
+            self.initialize(images)
+            
+        imag1_delta = self.translate(delta)
         mask = self.getmask(delta)
-        res = (imag1_delta - imag0)*mask
-        return res/np.sqrt(np.sum(mask))
+        res = (imag1_delta - self.imag0)*mask
+        return np.nan_to_num(res/np.sqrt(np.sum(mask)))
 
-    def model(self, delta, imag1=None):
+    def model(self, delta):
         """ Outputs imag1 shifted by delta"""
-        imag1_delta = self.translate(delta, imag1)
-        mask = self.getmask(delta)
-        return imag1_delta * mask
+        imag1_delta = self.translate(delta, self.imag1)
+        return imag1_delta * self.getmask(delta)
 
-    def res(self, delta, imag1=None, imag0=None):
-        """ Helper function which flattens the results of self.residual """
-        return self.residual(delta, imag1=None, imag0=None)
-
-    def cost(self, delta, imag1=None, imag0=None):
+    def cost(self, delta, images=None):
         """ The summed squared residual. See self.residual """
-        r = self.res(delta, imag1, imag0)
+        r = self.residual(delta, images)
         return r.dot(r)/2.
 
-    def gradres(self, delta, imag1=None, imag0=None):
-        res0 = self.res(delta, imag1, imag0)
-        return (self.res(delta+self._h, imag1, imag0)-res0)/self._h
+    def gradres(self, delta, images=None):
+        res0 = self.residual(delta, images)
+        return (self.residual(delta+self._h, images)-res0)/self._h
 
-    def firstguess(self, imag1=None, imag0=None):
+    def firstguess(self, images=None):
         """ 
         Naive estimation of the translation by simple cross-correlation
         input:
@@ -170,16 +167,17 @@ class Register(object):
         returns:
             delta : array_like of length 2 [dy, dx]
         """
-        imag1 = imag1 if imag1 is not None else self.imag1
-        imag0 = imag0 if imag0 is not None else self.imag0
-        assert imag1.shape==imag0.shape, "Images must share shapes"
+        if images is not None:
+            imag0, imag1 = images
+        else:
+            imag0, imag1 = self.imag0, self.imag1
         L = len(imag1)
         imag1_k, imag0_k = np.fft.rfft(imag1), np.fft.rfft(imag0)
         corr = np.fft.fftshift(np.fft.irfft(imag1_k*imag0_k.conj()))
         maxind = np.argmax(corr)
-        return - (maxind//L-L/2.)
+        return maxind - L/2.
 
-    def estimatenoise(self, delta_bestfit, imag1=None, imag0=None):
+    def estimatenoise(self, delta_bestfit, images=None):
         """
         Estimate the noise at the best fit.
         The log-likelihood is (including normalization)
@@ -195,11 +193,10 @@ class Register(object):
         returns:
             sigma : float, optimal sigma assuming best fit cost
         """
-        cost = self.cost(delta_bestfit, imag1, imag0)
+        cost = self.cost(delta_bestfit, images)
         return np.sqrt(2*cost)
 
-    def fit(self, delta0=None, imag1=None, imag0=None, method="Nelder-Mead",
-                 **kwargs):
+    def fit(self, images=None, delta0=None, **kwargs):
         """
         Register two images by optimizing translationg of imag1
         inputs:
@@ -218,13 +215,10 @@ class Register(object):
                 optimal delta), str or int (message from optimizer, see specific
                 optimizer for meaning)
         """
-        if imag1 is not None:
-            self.initialize(imag1)
-            self.imag1 = imag1
-        if imag0 is not None:
-            self.imag0 = imag0
+        if images is not None:
+            self.initialize(images)
 
-        delta0 = delta0 if delta0 is not None else self.firstguess(imag1, imag0)
+        delta0 = delta0 if delta0 is not None else self.firstguess()
         iprint = kwargs.get("iprint", 0)
 
         if kwargs.get('maskdict'):
@@ -232,11 +226,14 @@ class Register(object):
             self.masktype = maskdict.get('masktype', 'linear')
             self.mask_kwargs = maskdict.get('mask_kwargs', [{}, {}])
 
-        p1 = golden(self.cost, **kwargs)
+        #p1 = golden(self.cost, **kwargs)
+        self.sol = minimize_scalar(self.cost, bounds=(delta0-2., delta0+2.), 
+                                   method='bounded')
+        p1 = self.sol['x']
         j = self.gradres(p1)
         jtj = j.dot(j)
 
         mask = self.getmask(p1)
-        sigma = self.estimatenoise(p1, imag1, imag0)
+        sigma = self.estimatenoise(p1)
         p1_sigma = sigma/np.sqrt(jtj*np.sum(mask))
         return p1, p1_sigma
