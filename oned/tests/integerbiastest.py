b88d5bd7870b90a8e86161a1a53cb029f9279b1e
Colin Clement <colin.clement@gmail.com>
2017-10-15 15:29:18 -0400

    made testing more general to more easily compare methods

--- oned/tests/integerbiastest.py
+++ oned/tests/integerbiastest.py
@@ -16,6 +16,10 @@ import cPickle as pickle
 from scipy.misc import face
 import numpy as np
 import tester
+import makedata as md
+
+from onedregister import Register
+from fourier_register import SuperRegistration
 
 rng = np.random.RandomState(148509289)
                                                                       
@@ -44,18 +48,22 @@ if __name__=="__main__":
 
     today = datetime.today().isoformat().split("T")[0]
 
-    dataL = 128 
+    dataL = 256
+    img = md.randomblur((3*dataL, 3*dataL), 1., 4.)
     noise = 0.05
-    deg = 40
-    abscissa = deltas = np.linspace(0., 2., 40)
+    deg = 60
+    abscissa = np.linspace(0., 2., 40)
+    shifts = np.array([[[s, 0.]] for s in abscissa])
     xlabel = "True shift $\Delta_y$"
     padding = 6
-    N = 2000
+    N = 1000
 
     #directory = 'deltaresults/x-zero-s_{}-p_{}-N_{}-'.format(noise,padding,N)+today
 
-    datakwargs = [(2, dataL, noise), {'coef': rng.randn(2*deg-1)}]
-    mask_kwargs = {'sigmoid': {'o': padding, 'w': 0.5},
+    datakwargs = {'shifts': [np.zeros(2)], 'L': dataL,
+                  'sliceobj': np.s_[dataL:2*dataL,dataL+dataL/2],
+                  'img': img}
+    mask_kwargs = {'sigmoid': {'o': padding, 'w': 0.5, 'o': padding},
                    'constant': {'o': padding, 'l': dataL-2*padding},
                    'linear': {}}
 
@@ -67,11 +75,18 @@ if __name__=="__main__":
     fig, axes = plt.subplots(1, 3, sharex='all', figsize=(10,5)) 
 
     #for i, data in enumerate(datakwargs):
+    allresults = []
     for j, mask in enumerate(mask_kwargs):
         start = datetime.now()
-        biastest = tester.BiasTest(datakwargs, N=N, masktype=mask,
-                               mask_kwargs=mask_kwargs[mask])
-        results, alldata = biastest.deltaloop(deltas, noise)
+        biastest = tester.BiasTest(datakwargs, Register, N=N)
+        alldata = biastest.deltaloop(shifts, noise)
+        results = {'bias': [], 'biaserr': [], 'err': []}
+        for d, s in zip(alldata, shifts):
+            p1s, p1_sigmas = d
+            results['bias'] += [np.mean(p1s)-s[0,0]]
+            results['biaserr'] += [np.std(p1s)/np.sqrt(len(p1s))]
+            results['err'] += [np.mean(p1_sigmas)]
+        allresults += [results]
         f, a = biastest.plotbias(results, abscissa=abscissa, xlabel=xlabel, 
                                  axis=axes[j], 
                                  title="{} mask".format(mask))
