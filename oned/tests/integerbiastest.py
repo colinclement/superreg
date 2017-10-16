01aa116617654d7e465fcee67254d3fed775db4a
Colin Clement <colin.clement@gmail.com>
2017-10-16 00:01:40 -0400

    also tested superregistration

--- oned/tests/integerbiastest.py
+++ oned/tests/integerbiastest.py
@@ -49,14 +49,14 @@ if __name__=="__main__":
     today = datetime.today().isoformat().split("T")[0]
 
     dataL = 256
-    img = md.randomblur((3*dataL, 3*dataL), 1., 4.)
-    noise = 0.05
-    deg = 60
+    img = md.randomblur((3*dataL, 3*dataL), 1., 3.)
+    noise = 0.1
+    deg = 40
     abscissa = np.linspace(0., 2., 40)
     shifts = np.array([[[s, 0.]] for s in abscissa])
     xlabel = "True shift $\Delta_y$"
     padding = 6
-    N = 1000
+    N = 500
 
     #directory = 'deltaresults/x-zero-s_{}-p_{}-N_{}-'.format(noise,padding,N)+today
 
@@ -72,25 +72,42 @@ if __name__=="__main__":
     #except OSError as oerr:
     #    pass
 
-    fig, axes = plt.subplots(1, 3, sharex='all', figsize=(10,5)) 
+    fig, axes = plt.subplots(1, 2, sharex='all', figsize=(10,5)) 
 
-    #for i, data in enumerate(datakwargs):
     allresults = []
-    for j, mask in enumerate(mask_kwargs):
-        start = datetime.now()
-        biastest = tester.BiasTest(datakwargs, Register, N=N)
-        alldata = biastest.deltaloop(shifts, noise)
-        results = {'bias': [], 'biaserr': [], 'err': []}
-        for d, s in zip(alldata, shifts):
-            p1s, p1_sigmas = d
-            results['bias'] += [np.mean(p1s)-s[0,0]]
-            results['biaserr'] += [np.std(p1s)/np.sqrt(len(p1s))]
-            results['err'] += [np.mean(p1_sigmas)]
-        allresults += [results]
-        f, a = biastest.plotbias(results, abscissa=abscissa, xlabel=xlabel, 
-                                 axis=axes[j], 
-                                 title="{} mask".format(mask))
-        plt.suptitle("{} padding, N={}, $\sigma$={}".format(padding, N, noise))
+    #for j, mask in enumerate(mask_kwargs):
+    biastest = tester.BiasTest(datakwargs, Register, N=N, 
+                               mask_kwargs=mask_kwargs['sigmoid'])
+    biastest_sr = tester.BiasTest(datakwargs, SuperRegistration, deg, N=N)
+
+    start = datetime.now()
+    alldata = biastest.deltaloop(shifts, noise)
+    print("finished fourier shift in {}".format(datetime.now()-start))
+
+
+    start = datetime.now()
+    alldata_sr = biastest_sr.deltaloop(shifts, noise)
+    print("finished super registration in {}".format(datetime.now()-start))
+
+    results = {'bias': [], 'biaserr': [], 'err': []}
+    results_sr = {'bias': [], 'biaserr': [], 'err': []}
+    for d, dsr, s in zip(alldata, alldata_sr, shifts):
+        p1s, p1_sigmas = d
+        results['bias'] += [np.mean(p1s)-s[0,0]]
+        results['biaserr'] += [np.std(p1s)/np.sqrt(len(p1s))]
+        results['err'] += [np.mean(p1_sigmas)]
+
+        p1s, p1_sigmas = dsr
+        results_sr['bias'] += [np.mean(p1s)-s[0,0]]
+        results_sr['biaserr'] += [np.std(p1s)/np.sqrt(len(p1s))]
+        results_sr['err'] += [np.mean(p1_sigmas)]
+
+    f, a = biastest.plotbias(results, abscissa=abscissa, xlabel=xlabel, 
+                             axis=axes[0], title="Fourier Registration")
+    f, a = biastest.plotbias(results_sr, abscissa=abscissa, xlabel=xlabel, 
+                             axis=axes[1], title="Super Registration")
+                             #title="{} mask".format(mask))
+    plt.suptitle("N={}, $\sigma$={}".format(N, noise))
     #f.savefig(os.path.join(directory,"summary.pdf".format(data,mask)))
     #filename = os.path.join(directory,"data-{}_mask-{}.pkl".format(data,mask))
     #with open(filename, 'w') as outfile:
@@ -99,5 +116,5 @@ if __name__=="__main__":
     #    pickle.dump(datakwargs[data], outfile)
     #    pickle.dump(mask_kwargs[mask], outfile)
     #    pickle.dump(alldata, outfile)
-        print("finished {} in {}".format(mask, datetime.now()-start))
+    #print("finished {} in {}".format(mask, datetime.now()-start))
     plt.show()
