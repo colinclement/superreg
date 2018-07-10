adc2b52bf55916e2e19b2e0c87713000c8f43b42
Colin Clement <colin.clement@gmail.com>
2018-07-10 19:18:02 -0400

    complete test version for paper

--- super_reg/super_reg/twod/tests/nonperiodic-bias-noise.py
+++ super_reg/super_reg/twod/tests/nonperiodic-bias-noise.py
@@ -35,33 +35,6 @@ def checkdir(dirs):
 resultsdir = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'results')
 checkdir(resultsdir)
 
-def saveresults(filename, results, noises, datakwargs, mask_kwargs,
-                alldata, shifts):
-    with open(filename, "wb") as fout:
-        pickle.dump(results, fout)
-        pickle.dump(noises, fout)
-        pickle.dump(datakwargs, fout)
-        pickle.dump(mask_kwargs, fout)
-        pickle.dump(alldata, fout)
-        pickle.dump(shifts, fout)
-
-def loadresults(directory):
-    data = {}
-    filenames = glob.glob(os.path.join(directory, '*.pkl'))
-    for i, f in enumerate(filenames):
-        with open(f, "rb") as infile:
-            res = pickle.load(infile)
-            noises = pickle.load(infile)
-            datakwargs = pickle.load(infile)
-            mask_kwargs = pickle.load(infile)
-            alldata = pickle.load(infile)
-            shifts = pickle.load(infile)
-        name = os.path.split(f)[-1]
-        data[name] = {'results': res, 'noises': noises,
-                      'datakwargs': datakwargs, 'mask_kwargs': mask_kwargs,
-                      'alldata': alldata, 'shifts': shifts}
-    return data
-
 
 if __name__=="__main__":
     from datetime import datetime
@@ -71,16 +44,16 @@ if __name__=="__main__":
 
     today = datetime.today().isoformat().split("T")[0]
     
-    abscissa = np.linspace(0., 2., 16)
+    abscissa = np.linspace(0., 2., 20)
     shift = [rng.randn(2)]
     shifts = np.array([[[s, 0.]] for s in abscissa])
     noise = 0.05
     noises = np.linspace(5E-3, 0.1, 20)
     xlabel = "True shift $\Delta_y$"
-    N = 300
+    N = 1000
     L = 32
     padding = 3
-    deg = 15
+    deg = 13
 
     datakwargs = {'L': L, 'offset': np.zeros(2), 'shifts': shift,
                   'img': md.powerlaw((2*L, 2*L), 1.8, scale=L/4., rng=rng),
@@ -93,15 +66,31 @@ if __name__=="__main__":
     # noise degree gamma
 
     fstest = tester.BiasTest(datakwargs, Register, N=N, mask_kwargs=mask_kwargs,
-                             masktype='constant')
+                             masktype='constant', noises=noises)
+
     dcomp = complexities[np.argmin(np.abs(complexities[:,0]-noise))]
-    srtest = tester.BiasTest(datakwargs, cs.SuperRegistration, N=N, deg=deg)
+    srtest = tester.BiasTest(datakwargs, cs.SuperRegistration, N=N, deg=deg,
+                             noises=noises)
     #srtest = tester.BiasTest(datakwargs, csp.SRGaussianPriors, N=N, 
     #                         deg=deg, gamma=0.001)
 
-    fsdeltaloop = fstest.deltaloop(shifts, noise)
-    srdeltaloop = srtest.deltaloop(shifts, noise, delta=1e-8).squeeze()
+    start = datetime.now()
+    fsnoiseloop = fstest.noiseloop()
+    print("Finished FS noiseloop in {}".format(datetime.now()-start))
 
-    np.savez("results/shiftbiascomparison-d_{}-N_{}-{}".format(deg, N, today),
-             fs = fsdeltaloop, sr = srdeltaloop, noise=noise, shifts=shifts,
-             datakwargs=[datakwargs])
+    start = datetime.now()
+    fsdeltaloop = fstest.deltaloop(shifts, noise)
+    print("Finished FS deltaloop in {}".format(datetime.now()-start))
+
+    start = datetime.now()
+    srnoiseloop = srtest.noiseloop(delta=1e-8).squeeze()
+    print("Finished SR noiseloop in {}".format(datetime.now()-start))
+
+    start = datetime.now()
+    srdeltaloop = srtest.deltaloop(shifts, noise, delta=1e-8).squeeze()
+    print("Finished SR deltaloop in {}".format(datetime.now()-start))
+
+    np.savez("results/shiftnoisebiascomparison-d_{}-N_{}-{}".format(deg, N, today),
+             fsdelta=fsdeltaloop, srdelta=srdeltaloop, noise=noise,
+             noises=noises, shift=shift, shifts=shifts, fsnoise=fsnoiseloop,
+             srnoise=srnoiseloop, datakwargs=[datakwargs])
