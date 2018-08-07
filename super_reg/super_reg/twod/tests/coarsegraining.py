ce329edfbdf1cf0117d43e53e6d65dd8b61823cc
Colin Clement <colin.clement@gmail.com>
2018-08-06 20:44:58 -0400

    coarsegrain test refinement

--- super_reg/super_reg/twod/tests/coarsegraining.py
+++ super_reg/super_reg/twod/tests/coarsegraining.py
@@ -26,6 +26,18 @@ def checkdir(dirs):
     except OSError as e:
         pass
 
+def crb(I, sigma):
+    ly, lx = I.shape
+    Ik = np.fft.fftn(I, norm='ortho')  # Unitary norm!
+    ky = np.fft.fftfreq(ly, d=1./(2*np.pi))[:,None]
+    kx = np.fft.fftfreq(lx, d=1./(2*np.pi))
+    return sigma **2 / np.sum(ky**2 * Ik * Ik.conj()).real
+
+def predictvar(I, sigma, N=None):
+    s = crb(I, sigma)
+    N = N or I.size
+    return 2*s*(1+N*np.pi**2 * s/6)
+
 resultsdir = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'results')
 checkdir(resultsdir)
 
@@ -37,22 +49,30 @@ if __name__=="__main__":
 
     today = datetime.today().isoformat().split("T")[0]
     shift = [rng.randn(2)]
-    noises = np.linspace(5E-3, 0.1, 20)
-    N = 200
+    noises = np.linspace(5E-3, 0.1, 5)
+    N = 500
     L = 1024
-    coarsenings = range(1,9)
+    #coarsenings = range(1,9)  # non-powers of 2 lead to very inaccurate results
+    coarsenings = [1, 2, 4, 8, 16]
 
-    directory = 'results/coarsen-N_{}-L_{}-'.format(N, L)+today
+    saveloc = 'results/coarsen-N_{}-L_{}-'.format(N, L)+today
 
     datakwargs = {
         'L': L, 'offset': np.zeros(2), 'shifts': shift, 
-        'img': md.powerlaw((L, L), 1.8, scale=L/6., rng=rng), 'mirror': False
+        'img': md.powerlaw((L, L), 1.8, scale=L/64., rng=rng), 'mirror': False
     }
+    img = datakwargs['img']
     
-    os.makedirs(directory, exist_ok=True)
-
-    start = datetime.now()
     tester = BiasTest(datakwargs, N=N, registration=Register,
                       noises = noises)
 
+    # first do a standard noise loop to debug the first step of coarse graining
+    #noiseloop = tester.noiseloop()
+
+    start = datetime.now()
     data = tester.coarsegrain(coarsenings)
+    print("Finished testing coarse graining in {}".format(datetime.now()-start))
+
+    np.savez(saveloc, datakwargs=[datakwargs], noises=noises, 
+             shift=shift, data=data, coarsenings=list(coarsenings),
+             noiseloop=noiseloop)
