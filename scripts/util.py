bd3ffe74591c2862b921f8609dc7f5ba0a472b26
Colin Clement <colin.clement@gmail.com>
2018-09-04 14:00:58 -0400

    consolidated util more

--- scripts/util.py
+++ scripts/util.py
@@ -125,3 +125,16 @@ def reorder(images, L):
         else:
             output = np.concatenate([output, images[i*L:(i+1)*L][::-1]])
     return output
+
+def intshifts(data, shifts, size=64):
+    C = data.shape[-1]//2 - size//2
+    #C = shifts.mean(0)
+
+    s = shifts.astype('int')
+    out = [data[0][C:C+size, C:C+size]]
+
+    for i in range(len(shifts)):
+        d = data[i+1]
+        y, x = s[i]
+        out.append(d[C-y:C-y+size, C-x:C-x+size])
+    return np.array(out)
