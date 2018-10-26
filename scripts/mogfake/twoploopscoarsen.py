16b09dc753a0d74268646c1ad4db4fb83aaa8e58
Colin Clement <colin.clement@gmail.com>
2018-10-26 17:19:30 -0400

    two particle numerical experiment scripts

--- scripts/mogfake/twoploopscoarsen.py
+++ scripts/mogfake/twoploopscoarsen.py
@@ -32,76 +32,40 @@ def makemodel(shifts, pos, widths, **kwargs):
     return md.twoparticles(np.concatenate([[np.zeros(2)], shifts]), pos=pos,
                            widths=widths)
 
-def evidenceloop(data, sigma, deglist, **kwargs):
-    iprint = kwargs.pop('iprint', 0)
-    delta = kwargs.pop('delta', 1E-5)
-    lamb = kwargs.pop('lamb', .1)
-    evdlist = []
-    shiftlist = []
-    coeflist = []
-    
-    start = datetime.now()
-    for i, d in enumerate(deglist):
-        reg = SuperRegistration(data, d, **kwargs)
-        shiftlist.append(reg.fit(iprint=iprint, delta=delta, lamb=lamb))
-        evdlist.append(reg.evidence(sigma=sigma))
-        coeflist.append(reg.coef)
-        if iprint:
-            print("d = {} evd = {}".format(d, evdlist[-1]))
-    if iprint:
-        print("Calculation took {}".format(datetime.now()-start))
-    return evdlist, shiftlist, coeflist    
-
-def standardmethod(data, **kwargs):
-    fshift = util.firsttry(data, **kwargs)
+def standardmethod(data, a=1, **kwargs):
+    fshift = util.coarsereg(data, a, **kwargs)
     fsrecon = util.shiftallto(data, fshift)
     return fshift, fsrecon
 
-def noiseloop(sigma, model, params, pnames, p, M=20, deglist=range(5,20),
+def noiseloop(sigma, model, params, pnames, p, M=20, alist=[1, 2, 3, 4],
               **kwargs):
     iprint = kwargs.pop('iprint', 0)
     delta = kwargs.pop('delta', 1E-5)
     lamb = kwargs.pop('lamb', .1)
 
-    # find best evidence
-    data = model + sigma * rng.randn(*model.shape)
-    evdlist, shiftlist, coeflist = evidenceloop(data, sigma, deglist,
-                                                iprint=iprint)
-    # warn if peak is at edges
-    argmax = np.argmax(evdlist)
-    maxdeg = list(deglist)[argmax]
-    if argmax == 0 or argmax == len(evdlist)-1:
-        print("Peak evidence is at the edge of deglist")
-    # loop over noise
-    sr_results = []
     fs_results = []
-    print("\tStarting noise loop with deg={}".format(maxdeg))
     for i in range(M):
         data = model + sigma * rng.randn(*model.shape)
+        ares = []
+        for a in alist:
+            fshift, fsrecon = standardmethod(data, a)
+            fsmog = makemog(fsrecon, sigma, *params)
+            ares.append(fsmog.fit(
+                pnames, p, delta=1E-6, iprint=iprint
+            )[0])  # only keep best fits
+        fs_results.append(ares)
 
-        reg = SuperRegistration(data, maxdeg)
-        reg.fit(iprint=iprint, delta=delta, lamb=lamb)
-        srmog = makemog(reg.model[0], sigma, *params)
-        sr_results.append(srmog.fit(
-            pnames, p, delta=1E-6, iprint=iprint, solver='spsolve'
-        )[:2])
-
-        fshift, fsrecon = standardmethod(data)
-        fsmog = makemog(fsrecon, sigma, *params)
-        fs_results.append(fsmog.fit(
-            pnames, p, delta=1E-6, iprint=iprint, solver='spsolve'
-        )[:2])
-
-    return maxdeg, sr_results, fs_results
+    return fs_results
 
 
 if __name__ == '__main__':
     #Experimental setup
     M = 1000  # number of noise instances
     Nsigma = 20  # number of noise strengths
-
+    alist = [1, 2, 3, 4]
     N = 8
     L = 64
+
     s0 = rng.randn(N-1, 2)/(L/2)
     shifts = L * s0
     pos = [0.3*rng.rand(2)+0.0, 0.4*rng.rand(2)+0.5]
@@ -115,26 +79,23 @@ if __name__ == '__main__':
     pnames.remove('epsilon')
     pnames.remove('theta')
     p0 = tmog.pmap.getvalues(pnames)
-    truep = tmog.fit(pnames, p0, delta=1E-6, iprint=1, solver='cg')
+    truep = tmog.fit(pnames, p0, delta=1E-6)
 
     sigmalist = np.linspace(0.01, .3, Nsigma)
     results = []
-    maxdeglist = []
 
     start = datetime.now()
     for i, sigma in enumerate(sigmalist):
         print("Starting sigma={}".format(sigma))
-        deglist = range(maxdeg-2, maxdeg+3) if i else range(12, 16)
 
-        maxdeg, sr_results, fs_results = noiseloop(
-            sigma, model, params, pnames, p0, M=M, deglist=deglist,
+        fs_results = noiseloop(
+            sigma, model, params, pnames, p0, M=M, alist=alist
         )
-        maxdeglist.append(maxdeg)
-        results.append([sr_results, fs_results])
+        results.append(fs_results)
     print("Calculation finished in {}".format(datetime.now()-start))
 
-    savename = '{}-twop-M_{}-N_{}-Nsigma_{}-L_{}'.format(today, M, N, Nsigma, L)
+    savename = '{}-twop-coarsen-M_{}-N_{}-Nsigma_{}-L_{}'.format(today, M, N, Nsigma, L)
     saveloc = os.path.join('data', savename)
     np.savez(saveloc, M=M, Nsigma=Nsigma, N=N, L=L, shifts=shifts, s0=s0,
-             pos=pos, widths=widths, params=params, model=model,
+             pos=pos, widths=widths, params=params, model=model, alist=alist,
              sigmalist=sigmalist, results=results, truep=truep, pnames=pnames)
