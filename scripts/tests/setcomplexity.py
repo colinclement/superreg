6bbcac6ae670818b301c7a65646c465814c9c66f
Colin Clement <colin.clement@gmail.com>
2019-02-17 19:41:18 -0500

    removed unused code

--- scripts/tests/setcomplexity.py
+++ scripts/tests/setcomplexity.py
@@ -21,7 +21,7 @@ import superreg.util.tester as tester
 import superreg.util.makedata as md
 
 from superreg.fouriershift import Register
-import superreg.chebseriespriors as csp
+import superreg.chebseries as cs
 
 rng = np.random.RandomState(148509289)
 
@@ -34,32 +34,15 @@ def checkdir(dirs):
 resultsdir = os.path.join(os.path.split(os.path.realpath(__file__))[0], 'results')
 checkdir(resultsdir)
 
-def saveresults(filename, results, noises, datakwargs, mask_kwargs,
-                alldata, shifts):
-    with open(filename, "wb") as fout:
-        pickle.dump(results, fout)
-        pickle.dump(noises, fout)
-        pickle.dump(datakwargs, fout)
-        pickle.dump(mask_kwargs, fout)
-        pickle.dump(alldata, fout)
-        pickle.dump(shifts, fout)
-
-def loadresults(directory):
-    data = {}
-    filenames = glob.glob(os.path.join(directory, '*.pkl'))
-    for i, f in enumerate(filenames):
-        with open(f, "rb") as infile:
-            res = pickle.load(infile)
-            noises = pickle.load(infile)
-            datakwargs = pickle.load(infile)
-            mask_kwargs = pickle.load(infile)
-            alldata = pickle.load(infile)
-            shifts = pickle.load(infile)
-        name = os.path.split(f)[-1]
-        data[name] = {'results': res, 'noises': noises,
-                      'datakwargs': datakwargs, 'mask_kwargs': mask_kwargs,
-                      'alldata': alldata, 'shifts': shifts}
-    return data
+def find_all_complexities(datakwargs, noises
+    complexities = []
+    for n in noises:
+        data = md.fakedata(n, **datakwargs)
+        d, g = cs.optcomplexity(data, cs.SRGaussianPriors, sigma=n,
+                                show=True, delta=1E-6, tol=1E-4)
+        print(n, d, g)
+        complexities.append([n, d, g])
+    complexities = np.array(complexities)
 
 
 if __name__=="__main__":
@@ -83,18 +66,15 @@ if __name__=="__main__":
     datakwargs = {'L': L, 'offset': np.zeros(2), 'shifts': shifts[-1],
                   'img': md.powerlaw((2*L, 2*L), 1.8, scale=2*L/6., rng=rng),
                   'mirror': False}
-    mask_kwargs = [{'o': padding, 'l': L-2*padding}, 
-                   {'o': padding, 'l': L-2*padding}]
 
     complexities = []
-    gamma = 1E-2
-    #for n in noises:
-    #    data = md.fakedata(n, **datakwargs)
-    #    d, g = csp.optcomplexity(data, csp.SRGaussianPriors, sigma=n,
-    #                             show=True, delta=1E-6, tol=1E-4, gamma=gamma)
-    #    print(n, d, g)
-    #    complexities.append([n, d, g])
-    #complexities = np.array(complexities)
+    for n in noises:
+        data = md.fakedata(n, **datakwargs)
+        d, g = cs.optcomplexity(data, cs.SRGaussianPriors, sigma=n,
+                                show=True, delta=1E-6, tol=1E-4)
+        print(n, d, g)
+        complexities.append([n, d, g])
+    complexities = np.array(complexities)
 
-    #savename = "results/complexity-g-{}-{}-noise-degree-gamma.npy".format(gamma, today)
-    #np.save(savename, complexities)
+    savename = "results/complexity-{}-noise-degree.npy".format(today)
+    np.save(savename, complexities)
